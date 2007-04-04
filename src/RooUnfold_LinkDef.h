e030dcf6fc88dbcb00f2eec12cc96db14e62c0f9
adye <adye@f7cf43ea-45b0-4edf-8cd0-0019556f1211>
2007-04-04 21:10:53 +0000

    first standalone release

---
+++ src/RooUnfold_LinkDef.h
@@ -0,0 +1,18 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class vector<Double_t>+;
+#pragma link C++ class vector<Int_t>+;
+#pragma link C++ class Array2D+;
+#pragma link C++ class RooUnfoldBayesImpl+;
+#pragma link C++ class RooUnfold+;
+#pragma link C++ class RooUnfoldBayes+;
+#pragma link C++ class RooUnfoldSvd+;
+#pragma link C++ class RooUnfoldBinByBin+;
+#pragma link C++ class RooUnfoldResponse-;
+#pragma link C++ class TUnfHisto;
+
+#endif
