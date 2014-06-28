cd8003e3367fb18275d53959f2342d6450cc54e1
Lukasz Kreczko <lkreczko@googlemail.com>
2014-06-28 11:27:42 +0200

    made install.sh easier to read

--- ci/install.sh
+++ ci/install.sh
@@ -4,12 +4,9 @@
 # The behavior of the script is controlled by environment variabled defined
 # in the .travis.yml in the top level folder of the project.
 set -e
-if [[ $TRAVIS_PYTHON_VERSION == '3.2' ]]; then export PYTHON_SUFFIX="3"; fi
-
-# This is needed for the docs
-git submodule init
-git submodule update
-
+if [[ $TRAVIS_PYTHON_VERSION == 3* ]]; then
+    export PYTHON_SUFFIX="3"
+fi
 # add repositories for gcc 4.8 and clang 3.5
 sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
 sudo add-apt-repository --yes 'deb http://llvm.org/apt/precise/ llvm-toolchain-precise main'
@@ -18,10 +15,11 @@ wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | sudo apt-key add -
 time sudo apt-get update -qq
 
 # Install the dependencies we need
-time sudo apt-get -qq install clang-3.5 libclang-3.5-dev gcc-4.8 g++-4.8
-time sudo apt-get install -qq python${PYTHON_SUFFIX}-numpy python${PYTHON_SUFFIX}-sphinx python${PYTHON_SUFFIX}-nose
+time sudo apt-get install -qq python${PYTHON_SUFFIX}-numpy python${PYTHON_SUFFIX}-sphinx python${PYTHON_SUFFIX}-nose clang-3.5 libclang-3.5-dev gcc-4.8 g++-4.8
 # matplotlib and PyTables are not available for Python 3 as packages from the main repo yet.
-if [[ $TRAVIS_PYTHON_VERSION == '2.7' ]]; then time sudo apt-get install -qq python${PYTHON_SUFFIX}-matplotlib python${PYTHON_SUFFIX}-tables; fi
+if [[ $TRAVIS_PYTHON_VERSION == '2.7' ]]; then
+  time sudo apt-get install -qq python${PYTHON_SUFFIX}-matplotlib python${PYTHON_SUFFIX}-tables
+fi
 
 # Install a ROOT binary that we custom-built in a 64-bit Ubuntu VM
 # for the correct python / ROOT version
@@ -30,9 +28,12 @@ time tar zxf root_v${ROOT}_python_${TRAVIS_PYTHON_VERSION}.tar.gz
 mv root_v${ROOT}_python_${TRAVIS_PYTHON_VERSION} root
 source root/bin/thisroot.sh
 # setup newer compilers for ROOT 6
-if [[ $ROOT == '6-00-00' ]]; then sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50; fi
-if [[ $ROOT == '6-00-00' ]]; then sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 50; fi
-if [[ $ROOT == '6-00-00' ]]; then sudo update-alternatives --set gcc /usr/bin/gcc-4.8; sudo update-alternatives --set g++ /usr/bin/g++-4.8; fi
+if [[ $ROOT == 6* ]]; then
+  sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50
+  sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 50
+  sudo update-alternatives --set gcc /usr/bin/gcc-4.8
+  sudo update-alternatives --set g++ /usr/bin/g++-4.8
+fi
 
 # compile RooUnfold
 make
